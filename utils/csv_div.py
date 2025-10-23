1a67ae7437e307240315ee3e314ad7250f0fcab2
junkingboy <junkingboy@163.com>
2025-10-24 01:08:25 +0800

    [fix] 修复元数据签发方法.无需对签名进行加密.直接调用签名生成方法即可 [add] 添加文件校验逻辑

--- utils/csv_div.py
+++ utils/csv_div.py
@@ -1,4 +1,3 @@
-import io
 import os
 import csv
 import threading
@@ -13,6 +12,9 @@ from enums.loglabelEnum import LogLabelEnum
 
 class CsvCore:
     '''
+    TODO:
+    1.完善解析文件方法
+    2.思考是否有必要对csv进行签发、验证
     该类的职责:
     1.负责csv文件的签名发放
     2.负责csv文件的签名验证
@@ -53,17 +55,16 @@ class CsvCore:
             self._e.error("%s 初始化加密密钥失败", LogLabelEnum.ERROR.value)
             return
         # 加密表头
-        csv_header: str = CsvHeaderEnum.get_headers_str()
         csv_header_list: list = CsvHeaderEnum.get_headers_list()
-        sig_str: str = self._key_manager.generate_encry_str(csv_header)
+        encry_sig: str = self._key_manager.generate_signature_str(csv_header_list)
         # 加密公钥
         encry_pub: str = self._key_manager.generate_encry_str(self._key_manager.rsa_pub_key)
         # 元数据写入csv
         try:
             with open(self._csv_template, "w", encoding="utf-8", newline="") as f:
                 csv_w = csv.writer(f)
-                csv_w.writerow([CsvMetaEnum.SIG.value, sig_str])
-                csv_w.writerow([CsvMetaEnum.PUB_ENCRY_KEY.value, encry_pub])
+                csv_w.writerow([f"#{CsvMetaEnum.SIG.value}:{encry_sig}"])
+                csv_w.writerow([f"#{CsvMetaEnum.PUB_ENCRY_KEY.value}:{encry_pub}"])
                 csv_w.writerow(csv_header_list)
             self._e.info("%s 初始化csv元数据成功,csv模板已生成: %s", LogLabelEnum.SUCCESS.value, self._csv_template)
             return
@@ -72,3 +73,41 @@ class CsvCore:
             self._e.error("%s 写入csv元数据失败", LogLabelEnum.ERROR.value)
             return
 
+    def _is_source_csv(self, csv_file: str) -> bool:
+        if not csv_file: return False
+        if not os.path.exists(csv_file):
+            self._e.error("%s csv文件不存在", LogLabelEnum.ERROR.value)
+            return False
+        try:
+            csv_bytes: bytes = Path(csv_file).read_bytes()
+            lines: list = csv_bytes.decode("utf-8").splitlines()
+            if len(lines) < 3:
+                self._e.error("%s csv文件格式错误", LogLabelEnum.UNSPORTED.value)
+                return False
+            reader = csv.reader(lines)
+            meta_sig: str = ""
+            meta_pub: str = ""
+            meta_header: list = []
+            # 只取前三行数据进行数据校验
+            for row in reader:
+                if row[0].startswith(f"#{CsvMetaEnum.SIG.value}"): meta_sig = row[0].split(":")[1].strip()
+                elif row[0].startswith(f"#{CsvMetaEnum.PUB_ENCRY_KEY.value}"): meta_pub = row[0].split(":")[1].strip()
+                elif row[0].startswith("#"): continue
+                else: meta_header = row
+            if not meta_sig or not meta_pub or not meta_header:
+                self._e.error("%s csv文件错误", LogLabelEnum.UNSPORTED.value)
+                return False
+            # 验证公钥是否相同
+            if self._key_manager.parse_encry_str(meta_pub) != self._key_manager.rsa_pub_key:
+                self._e.error("%s csv文件公钥错误", LogLabelEnum.ERROR.value)
+                return False
+            # 验证签名
+            if not self._key_manager.verify_signature(meta_header, meta_sig):
+                self._e.error("%s csv文件签名错误", LogLabelEnum.ERROR.value)
+                return False
+            self._e.info("%s csv文件验证成功", LogLabelEnum.SIGNATURE.value)
+            return True
+        except Exception as err:
+            self._e.handle_exception(err)
+            self._e.error("%s 验证csv文件失败", LogLabelEnum.ERROR.value)
+            return False
