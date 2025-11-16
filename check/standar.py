6823c302bb5a722e714589ba89d4804051dbba10
junkingboy <junkingboy@163.com>
2025-11-16 17:18:32 +0800

    [add] 新增标准业务处理方法 [mod] 修改标准请求头模板

---
+++ check/standar.py
@@ -0,0 +1,54 @@
+from requests import Response
+
+from utils.logs import ExceptionLog
+from template.httpTemplate import StandardReqDataTemplate
+from template.nosqlTemplate import UserData, MetaUserData
+from enums.loglabelEnum import LogLabelEnum
+from enums.serverEnum import ServerEnum
+
+def standard_normal_check(
+    real_resp: Response,
+    parse_resp: dict | str | None,
+    data: StandardReqDataTemplate
+) -> UserData | None:
+    e: ExceptionLog = ExceptionLog.get_instance()
+    if not isinstance(real_resp, Response):
+        e.info("%s 真实响应类型错误,响应类型为: %s", LogLabelEnum.ERROR.value, type(real_resp))
+        return
+    if not isinstance(data, StandardReqDataTemplate):
+        e.info("%s 请求参数类型错误,传入类型为: %s", LogLabelEnum.ERROR.value, type(data))
+        return
+    if real_resp.status_code != 200:
+        e.error("%s 请求失败,服务端状态码: %s", LogLabelEnum.ERROR.value, real_resp.status_code)
+        return
+    if parse_resp is None:
+        e.error("%s 解析响应信息失败", LogLabelEnum.ERROR.value)
+        return
+    # 响应解析
+    match parse_resp:
+        case dict() as resp:
+            res_code: int = resp.get("code", 0)
+            if res_code != ServerEnum.SUCCESS.value:
+                e.error("%s 登录失败,服务端状态码: %s", LogLabelEnum.ERROR.value, res_code)
+                return
+            e.info("%s 用户登录成功", LogLabelEnum.SUCCESS.value)
+            auth: str = resp.get("data", {}).get("token")
+            username: str = data.body.get("phone") # type: ignore
+            password: str = data.body.get("password") # type: ignore
+            if not auth:
+                e.info("%s 用户登录成功,获取token失败", LogLabelEnum.INFO.value)
+                return
+            ins_data: UserData = UserData(
+                username=username,
+                metadata=MetaUserData(
+                    password=password,
+                    Authorization=auth
+                )
+            )
+            return ins_data
+        case str() as resp:
+            e.info("%s 响应数据类型解析失败,响应数据类型为: %s, 响应数据为: %s", LogLabelEnum.ERROR.value, type(parse_resp), parse_resp)
+            return
+        case _:
+            e.error("%s 响应数据类型错误,响应数据类型为: %s", LogLabelEnum.ERROR.value, type(parse_resp))
+            return
--- template/httpTemplate.py
+++ template/httpTemplate.py
@@ -1,7 +1,10 @@
-from dataclasses import dataclass
+import copy
+
+from typing import Any
+from dataclasses import dataclass, fields
 
 @dataclass
-class HttpReqTemplate:
+class StandardReqHeaderSetTemplate:
     url: str
     method: str
     params: dict | None
@@ -10,4 +13,54 @@ class HttpReqTemplate:
 
     @property
     def info(self) -> dict:
-        return self.__dict__.copy()
+        return copy.deepcopy(self.__dict__)
+
+@dataclass
+class StandardReqDataTemplate:
+    url: str
+    method: str
+    params: dict | None
+    headers: dict | None
+    form: dict | None
+    body: dict | None
+
+    @property
+    def info(self) -> dict:
+        res: dict = copy.deepcopy(self.__dict__)
+        if self.form is None:
+            res.pop("form", None)
+        return res
+
+    def set_attr(
+        self,
+        attr_name: str,
+        attr_val: Any
+    ) -> bool:
+        dict_attrs_key: set[str] = {
+            "headers",
+            "params",
+            "body"
+        }
+
+        field_names: set[str] = set()
+
+        for field in fields(self):
+            field_names.add(field.name)
+        
+        if str(attr_name) not in field_names: setattr(self, str(attr_name), attr_val)
+        elif not hasattr(self, str(attr_name)): setattr(self, str(attr_name), attr_val)
+        else:
+            curr_val: str | dict = getattr(self, str(attr_name))
+            if (
+                str(attr_name) in dict_attrs_key and
+                isinstance(curr_val, dict) and
+                isinstance(attr_val, dict)
+            ):
+                # 重新赋值
+                curr_val.clear()
+                # 合并键值对
+                for k, v in attr_val.items():
+                    curr_val.setdefault(k, v)
+            else:
+                setattr(self, str(attr_name), attr_val)
+        return True
